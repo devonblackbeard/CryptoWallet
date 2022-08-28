a45f8c0a5acb8ebada91b8c2549f30335f3106f9
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2022-08-27 20:49:49 -0600

    Enable multidex auto gen file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cryptowallet/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CryptoWallet());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
