98678eb5a21f340494d2053e3c385af5efecb50a
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2022-08-27 20:48:20 -0600

    Initialize app with skeleton code

---
+++ lib/ui/authentication.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  State<Authentication> createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(color: Colors.blueAccent),
+        child: Text("Hello crypto world"),
+      ),
+    );
+  }
+}
